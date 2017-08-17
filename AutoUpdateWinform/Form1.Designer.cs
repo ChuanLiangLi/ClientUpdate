38d5b6797166318a30f6478bd95187a6e0aa3b33
erbing <1536007949@qq.com>
2017-08-17 19:09:51 +0800

    创建客户端项目

---
+++ AutoUpdateWinform/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AutoUpdateWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labDownFile = new System.Windows.Forms.Label();
+            this.labFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labDownFile
+            // 
+            this.labDownFile.AutoSize = true;
+            this.labDownFile.Location = new System.Drawing.Point(116, 233);
+            this.labDownFile.Name = "labDownFile";
+            this.labDownFile.Size = new System.Drawing.Size(55, 15);
+            this.labDownFile.TabIndex = 0;
+            this.labDownFile.Text = "label1";
+            // 
+            // labFileName
+            // 
+            this.labFileName.AutoSize = true;
+            this.labFileName.Location = new System.Drawing.Point(135, 64);
+            this.labFileName.Name = "labFileName";
+            this.labFileName.Size = new System.Drawing.Size(55, 15);
+            this.labFileName.TabIndex = 1;
+            this.labFileName.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(425, 158);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(85, 325);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(484, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(425, 233);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 5;
+            this.btnDownLoad.Text = "下载文件";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(532, 233);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "更新文件";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 375);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labFileName);
+            this.Controls.Add(this.labDownFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labDownFile;
+        private System.Windows.Forms.Label labFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
